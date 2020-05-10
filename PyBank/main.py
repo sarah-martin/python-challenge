b1094370ce06a735f8c5482377e578db0d01986b
sarah-martin <smartin@globaltieskc.org>
2020-05-09 23:10:51 -0500

    Final submission

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,11 @@
+#Incorporate csv module
 import os
 import csv
 
 #Create path to collect data from CSV file
 budget_csv=os.path.join('Resources', 'budget_data.csv')
 
-#Read in the csv file and skip the header
+#Read in the csv file and store the header
 with open(budget_csv) as csvfile:
     csvreader=csv.reader(csvfile,delimiter=",")
 
@@ -28,18 +29,24 @@ with open(budget_csv) as csvfile:
         #Determine total net amount of profit
         totalProfit=sum(monthlyProfit)
 
+    #loop through monthly profit column
     for x in range(totalMonths-1):
+
+        #add the monthly change based off current cell and next cell    
         monthlyChange.append(monthlyProfit[x]-monthlyProfit[x-1])
+
+        #use monthly change to calculate average change, max change, and min change
         avgChange=round(sum(monthlyChange)/(totalMonths-1),2)
         maxChange=max(monthlyChange)
         minChange=min(monthlyChange)
         
+        #find and store the date associate with the min and max changes
         if minChange==monthlyChange[x]:
             minChange_date=months[x]
         if maxChange==monthlyChange[x]:
             maxChange_date=months[x]
 
-    #Print findings
+    #Print findings in terminal
     print("Financial Analysis")
     print("-------------------------")
     print(f"Total Months: {totalMonths}")
@@ -55,7 +62,7 @@ with open(budget_csv) as csvfile:
     #Write in the new file
     with open(output_path, "w") as txtfile:
 
-        #Write headers
+        #Write findings in txt file
         txtfile.write("Financial Analysis\n")
         txtfile.write("-------------------------\n")
         txtfile.write(f"Total Months: {totalMonths}\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,18 +1,74 @@
+#Incorporate csv module
 import os
 import csv
 
+#Create file path
 pypoll_csv=os.path.join('Resources','election_data.csv')
 
+#Read the csv file and store the header
 with open(pypoll_csv) as csvfile:
-    csvreader=csv.reader(csvfile, delimiter=,",")
+    csvreader=csv.reader(csvfile, delimiter=",")
     header=next(csvreader)
 
-voterID=[]
-candidates=[]
+    #Create variables to hold data
+    totalVotes=[]
+    canidateName=[]
+    canidateOptions=[]
+    canidateVotes=[]
+    canidatePercent=[]
 
-for row in csvreader:
-    voterID.append(row[0])
-    candidates.append(row[2])
+    #For each row,
+    for row in csvreader:
 
-    #Count total number of cast votes using len
-    votesCast=len(voterID)
+        #Store total votes
+        totalVotes.append(row[2])
+
+        #Count total number of cast votes using len to count the list length
+        votesCast=len(totalVotes)
+
+    for canidateName in totalVotes:
+
+        #Store the canidate name from each row
+        canidateName=row[2]
+
+        #If the canidate is not already in the list,
+        if canidateName not in canidateOptions:
+
+            #Add them to the list of options and begin counting their votes
+            canidateOptions.append(canidateName)
+            canidateVotes.append(0)
+            canidatePercent.append(0)
+
+        for x in range(len(canidateOptions)):
+            if canidateOptions[x]==canidateName:
+                canidateVotes[x]+=1
+
+    #Loop through canidate votes to find the winner
+    for x in range(len(canidatePercent)):
+        canidatePercent[x]=round((canidateVotes[x]/votesCast)*100,3)
+
+        if max(canidateVotes)==canidateVotes[x]:
+            winner=canidateOptions[x]
+
+    #Print results to terminal
+    print("Election Results")
+    print("---------------------")
+    print(f"Total Votes: {votesCast}")
+    print("---------------------")
+    for x in range(len(canidateOptions)):
+        print(f"{canidateOptions[x]}: {canidatePercent[x]}% ({canidateVotes[x]})")
+    print("---------------------")
+    
+    #write results in text file
+    #specify text file path
+    output_path=os.path.join('Resources', 'main.txt')
+
+    #Open file for writing
+    with open(output_path, "w") as txtfile:
+        txtfile.write("Election Results\n")
+        txtfile.write("---------------------\n")
+        txtfile.write(f"Total Votes: {votesCast}\n")
+        txtfile.write("---------------------\n")
+        for x in range(len(canidateOptions)):
+            txtfile.write(f"{canidateOptions[x]}: {canidatePercent[x]}% ({canidateVotes[x]})\n")
+        txtfile.write("---------------------\n")
